2804dd60e3573ae1bff6e1faec045ae03ae6e3e8
davidS <120366113@umail.com>
2022-12-02 10:55:41 +0000

    Implementation code added to the Colour class that creates a constructor that takes three float parameters, getters for the RGB values and variables. The test passed.

--- src/main/java/TTDcolourTest/Ass03/Colour.java
+++ src/main/java/TTDcolourTest/Ass03/Colour.java
@@ -1,4 +1,24 @@
 package TTDcolourTest.Ass03;
 
 public class Colour {
+    private float Red;
+    private float Green;
+    private float Blue;
+
+    public Colour(float Red, float Green, float Blue) {
+        this.Red = Red;
+        this.Green = Green;
+        this.Blue = Blue;
+    }
+    public float getRed() {
+        return Red;
+    }
+
+    public float getGreen() {
+        return Green;
+    }
+
+    public float getBlue() {
+        return Blue;
+    }
 }
